facce9af5356806dcd15c4028f0419588c7a48ec
Mostafa Ewis <64862002+MostafaEwis@users.noreply.github.com>
2025-05-23 21:37:00 +0300

    added regular ACO and some sliders for control removed phermone and used only q as my TA pointed to me

--- sketch.js
+++ sketch.js
@@ -4,19 +4,25 @@
 // the ants will select a random direction of the 8 depending on the phermone
 // for now the ants will be colored red and the dots will be colored green
 //TODO:
-//I have to create a food source
-//when the ants reach the food source it shrinks
-//the ant should go back home after food source has been used
+//I have to create a food source [x]
+//when the ants reach the food source it shrinks [ ]
+//the ant should go back home after food source has been used [x]
+//the ability to add food source anywhere [ ]
+//the dimater of the grid for some reason is always a dead link [ ]
+//improvement on how to draw the food and the start position to be dynamic, lots of hard coding should be removed [ ]
+//the animation of the ant movement should be added for a smoother simulation [ ]
+//the program halts a lot in the simulation because when the ants need to update the phermone reguralry [ ]
+//make a version for the mobile
 let dotD = 20;
 let antNum = 100;
-let phermone = 100;
 let gridLen = 20;
-let rate = 0.9
-let q = 300
-let fps = 60
+let rate = 0.9;
+let q = 30000;
+let fps = 60;
 let decayLimit = 10;
+let traditional = false;
 class Dot{
-	directions = Array(8).fill(1);
+	directions = Array(8).fill(10);
 	num;
 	pos;
 	constructor(x, y, n){
@@ -32,7 +38,7 @@ class Dot{
 	}
 	draw(){
 		fill('green');
-		circle(this.pos.x, this.pos.y, dotD);
+		crcle(this.pos.x, this.pos.y, dotD);
 	}
 }
 class Grid{
@@ -95,22 +101,22 @@ class Grid{
 			let {r, c} = this.getRC(i);
 			stroke('purple');
 			if(r + 1 < gridLen){
-				strokeWeight(map(this.dots[i].directions[5], 0, 200, 0.1, 3));
+				strokeWeight(map(this.dots[i].directions[5], 0, 400, 0.1, 10, true));
 				line(this.dots[i].pos.x, this.dots[i].pos.y, 
 					this.dots[this.getN(r + 1, c)].pos.x, this.dots[this.getN(r + 1, c)].pos.y);
 			}
 			if(c + 1 < gridLen){
-				strokeWeight(map(this.dots[i].directions[3], 0, 200, 0.1, 3));
+				strokeWeight(map(this.dots[i].directions[3], 0, 400, 0.1, 10, true));
 				line(this.dots[i].pos.x, this.dots[i].pos.y, 
 					this.dots[this.getN(r, c + 1)].pos.x, this.dots[this.getN(r, c + 1)].pos.y);
 			}
 			if(c + 1 < gridLen && r + 1 < gridLen){
-				strokeWeight(map(this.dots[i].directions[4], 0, 200, 0.1, 3));
+				strokeWeight(map(this.dots[i].directions[4], 0, 400, 0.1, 10, true));
 				line(this.dots[i].pos.x, this.dots[i].pos.y, 
 					this.dots[this.getN(r + 1, c + 1)].pos.x, this.dots[this.getN(r + 1, c + 1)].pos.y);
 			}
 			if(c - 1 < gridLen && r + 1 < gridLen){
-				strokeWeight(map(this.dots[i].directions[6], 0, 200, 0.1, 3));
+				strokeWeight(map(this.dots[i].directions[6], 0, 400, 0.1, 10, true));
 				line(this.dots[i].pos.x, this.dots[i].pos.y, 
 					this.dots[this.getN(r + 1, c - 1)].pos.x, this.dots[this.getN(r + 1, c - 1)].pos.y);
 			}
@@ -121,7 +127,8 @@ class Ant{
 	currentNum;
 	currentPath = [];
 	gridRef;
-	foundFood = true;
+	searchForFood = true;
+	goAround = true;
 	constructor(n, gridRef){
 		this.currentNum = n;
 		this.gridRef = gridRef;
@@ -171,65 +178,74 @@ class Ant{
 		return dir;
 	}
 	move(){
-		let dir = this.pickDir();
-		//choosing the next cell part
-		let r = parseInt(this.currentNum / gridLen);
-		let c = this.currentNum % gridLen;
-		//console.log("r, c, n: ", r, c, this.currentNum);
-		//0------1------2
-		//|		|
-		//|		|
-		//7	 .	3
-		//|		|
-		//|		|
-		//6------5------4
-		if(dir == 0){
-			r--;
-			c--;
-		}else if(dir == 1){
-			r--;
-		}else if(dir == 2){
-			r--;
-			c++;
-		}else if(dir == 3){
-			c++;
-		}else if(dir == 4){
-			r++;
-			c++;
-		}else if(dir == 5){
-			r++;
-		}else if(dir == 6){
-			r++;
-			c--;
-		}else if(dir == 7){
-			c--;
-		}
-		this.currentPath.push([this.currentNum, dir]);
-		this.currentNum = r * gridLen + c;	
-		if(this.foundFood){
-			if(r <= gridLen - 1 && r >= gridLen - 3 && c <= gridLen - 1 && c >= gridLen - 3){
-				this.currentPath.forEach(arr =>{
-					this.layPhermone(arr[0], arr[1]);
-				})
-				this.currentPath = [];
-				this.foundFood = false;
-				this.gridRef.decay();
+		if(this.goAround){
+			let dir = this.pickDir();
+			//choosing the next cell part
+			let r = parseInt(this.currentNum / gridLen);
+			let c = this.currentNum % gridLen;
+			//console.log("r, c, n: ", r, c, this.currentNum);
+			//0------1------2
+			//|		|
+			//|		|
+			//7	 .	3
+			//|		|
+			//|		|
+			//6------5------4
+			if(dir == 0){
+				r--;
+				c--;
+			}else if(dir == 1){
+				r--;
+			}else if(dir == 2){
+				r--;
+				c++;
+			}else if(dir == 3){
+				c++;
+			}else if(dir == 4){
+				r++;
+				c++;
+			}else if(dir == 5){
+				r++;
+			}else if(dir == 6){
+				r++;
+				c--;
+			}else if(dir == 7){
+				c--;
 			}
-		}else{
-			if(r <= 2 && r >= 0 && c <= 2 && c >= 0){
-				this.currentPath.forEach(arr =>{
-					this.layPhermone(arr[0], arr[1]);
-				})
-				this.currentPath = [];
-				this.foundFood = true;
-				this.gridRef.decay();
+			this.currentPath.push([this.currentNum, dir]);
+			this.currentNum = r * gridLen + c;	
+			if(traditional){
+				if(r <= gridLen - 1 && r >= gridLen - 6 && c <= gridLen - 1 && c >= gridLen - 6){
+					this.goAround = false;
+				//	console.log(Colony.antsAtFoodSource);
+					Colony.antsAtFoodSource++;
+				}
 			}
-		}
-		//console.log("r, c, n: ", r, c, this.currentNum);
+			else if(this.searchForFood){
+				if(r <= gridLen - 1 && r >= gridLen - 3 && c <= gridLen - 1 && c >= gridLen - 3){
+					this.currentPath.forEach(arr =>{
+						this.layPhermone(arr[0], arr[1]);
+					})
+					this.currentPath = [];
+					this.searchForFood = false;
+					this.gridRef.decay();
+				}
+			}else{
+				if(r <= 2 && r >= 0 && c <= 2 && c >= 0){
+					this.currentPath.forEach(arr =>{
+						this.layPhermone(arr[0], arr[1]);
+					})
+					this.currentPath = [];
+					this.searchForFood = true;
+					this.gridRef.decay();
+				}
+			}
+			//console.log("r, c, n: ", r, c, this.currentNum);
 
+		}
 	}
 	layPhermone(n, dir){
-		this.gridRef.dots[n].directions[dir] += q / this.currentPath.length * phermone;
+		this.gridRef.dots[n].directions[dir] += q / this.currentPath.length;
 	}
 	draw(){
 		noStroke();
@@ -245,39 +261,87 @@ class Ant{
 }
 class Colony{
 	ants = [];
+	static antsAtFoodSource = 0;
 	constructor(gridRef){
 		for(let i = 0; i < antNum; i++){
 			this.ants[i] = new Ant(0, gridRef);
 		}
+	}
+	backToHome(){
+		let gridRef = this.ants[0].gridRef;
+		this.ants.forEach(ant => {
+			ant.currentPath.forEach(arr => {
+				ant.layPhermone(arr[0], arr[1]);
+				})
+			ant.currentPath = [];
+			ant.goAround = true;
+			ant.currentNum = 0;
+		});
+		gridRef.decay();
 	}
 	draw(){
 		this.ants.forEach(ant => ant.draw());
 	}
 	induce(){
 		this.ants.forEach(ant => ant.move());
+		if(traditional && Colony.antsAtFoodSource == antNum){
+			this.backToHome();
+			Colony.antsAtFoodSource = 0;
+
+		}
 	}
 }
 let colony;
 let grid;
 let img;
-function preload(){
-}
+let fpsSlider;
+let fpsP;
+let rateSlider;
+let rateP;
+let traditionalCheck;
+let traditionalCheckP;
 function setup() {
 	createCanvas(700, 700);
 	grid = new Grid(gridLen ** 2);
 	colony = new Colony(grid);
+	fpsSlider = createSlider(1, 60, 30, 1);
+	fpsSlider.position(840, 80);
+	fpsP = createP("fps");
+	fpsP.position(720, 65);
+	fpsSlider.size(120);
+	rateSlider= createSlider(0.01, 1, 0.9, 0.01);
+	rateSlider.position(840, 100);
+	rateP = createP("evaporation");
+	rateP .position(720, 85);
+	rateSlider.size(120);
+	traditionalCheck = createCheckbox("", true);
+	traditionalCheck.position(840, 60);
+	traditionalCheckP = createP("ACO");
+	traditionalCheckP.position(720, 45);
 }
 function draw() {
+	fps = fpsSlider.value();
+	fpsP.html("fps: " + fpsSlider.value())
+	rate = rateSlider.value();
+	rateP.html("evaporation: " + rateSlider.value())
+	traditional = traditionalCheck.checked();
 	background("pink");
+	fill('green');
+	rect(1.5 * dotD, 1.5 * dotD, height / gridLen);
+	fill('yellow');
+	// this need to improved, nevertheless I'm using n + 1 offset, where n is the number of cells to be regarded as food
+	rect(width -  6 * 1.5 * dotD, height  - 6 * 1.5 * dotD, 5 * height / gridLen);
 	stroke('purple');
 	//grid.draw();
 	grid.drawRelation();
-	fill('yellow');
-	circle(width - gridLen * 3, height - gridLen * 3, gridLen * 5);
-	fill('green');
-	circle(gridLen * 3, gridLen * 3, gridLen * 5);
 	colony.draw();
+	text(200, 700, 200);
 	if(!mouseIsPressed)
 		colony.induce();
 	frameRate(fps);
+}
+function keyPressed() {
+  if (key === 's') {
+    saveGif('mySketch', 120);
+  }
 }
